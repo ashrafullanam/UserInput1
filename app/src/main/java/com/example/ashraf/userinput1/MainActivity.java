addadd44ba829987c5c1f6ddcc9b82c3a4bcb608
ashrafull <ashrafullanam95@gmail.com>
2017-08-26 10:20:13 +0600

    Test

---
+++ app/src/main/java/com/example/ashraf/userinput1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.ashraf.userinput1;
+
+        import android.graphics.Color;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        TextView textView = new TextView(this);
+//        textView.setText("Alve");
+//        textView.setTextSize(50);
+//        textView.setTextColor(Color.BLUE);
+//        int maxline = textView.getMaxLines();
+//        textView.setText(textView.getText().toString()+maxline);
+
+        setContentView(R.layout.activity_main);
+    }
+}
